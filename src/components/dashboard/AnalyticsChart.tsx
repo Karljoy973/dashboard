2359ea22b24abc390fe14245f2bd542295b42755
973Karljoy <karljoyeux@gmail.com>
2025-01-26 22:14:27 +0100

    chart is working

--- src/components/dashboard/AnalyticsChart.tsx
+++ src/components/dashboard/AnalyticsChart.tsx
@@ -1,6 +1,32 @@
+'use client';
+import {
+    LineChart, Line, XAxis, YAxis, CartesianGrid, ResponsiveContainer
+} from 'recharts'
+import {
+    Card, CardContent, CardDescription, CardTitle, CardHeader
+} from "@/components/ui/card"
+import data from '@/data/analytics';
+
 const AnalyticsChart = () => {
     return (<>
-        <div>chart</div>
+        <Card>
+            <CardHeader>
+                <CardTitle>Analytics for this yrear</CardTitle>
+                <CardDescription>Views per months</CardDescription>
+                <CardContent>
+                    <div style={{ width: '100%', height: 300 }}>
+                        <ResponsiveContainer>
+                            <LineChart width={1100} height={300} data={data}>
+                                <Line type="monotone" dataKey="uniqueViews" stroke='#6684d8' />
+                                <CartesianGrid stroke="#ccc" />
+                                <XAxis dataKey="name"/>
+                                <YAxis />
+                            </LineChart>
+                        </ResponsiveContainer>
+                    </div>
+                </CardContent>
+            </CardHeader>
+        </Card>
     </>);
 }
  
